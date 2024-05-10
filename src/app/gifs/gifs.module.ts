af0f448ba82cf01e8b03d8306693d65b6b6b9d55
Juan Cremades <relicary@gmail.com>
2024-05-10 14:50:48 +0200

    Created SearchBox component inside the Gifs Module
    Refs #1

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    SearchBoxComponent
   ],
   imports: [
     CommonModule
