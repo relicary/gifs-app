aafcacd141802d15f427b240a3aba577e5302e5d
Juan Cremades <relicary@gmail.com>
2024-05-11 20:01:13 +0200

    The Gifs Service validates the Search Box input before store it
    Refs #7

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,25 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
-  public searchTag(tag: string) : void {
+  private organizeHistory(tag: string): void {
+
+    tag = tag.toLowerCase();
+
+    if (this.tagsHistory.includes(tag)) {
+      this._tagsHistory = this.tagsHistory.filter( (oldTag) => oldTag !== tag);
+    }
+
     this._tagsHistory.unshift(tag);
+
+    this._tagsHistory = this.tagsHistory.splice(0, 10);
+
+  }
+
+  public searchTag(tag: string) : void {
+
+    if ( tag.length === 0) return;
+
+    this.organizeHistory(tag);
+
   }
 }
